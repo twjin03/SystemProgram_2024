61ee5a84514e04374a83a66213870ef9bc76805b
twjin03 <twjin03@g.skku.edu>
2025-03-19 01:55:56 +0900

    Update hpfp.c
    addition function

--- hpfp.c
+++ hpfp.c
@@ -139,229 +139,43 @@ hpfp addition_function(hpfp a, hpfp b)
   unsigned short expB = (b >> 10) & 0x1F; // exponent bits 추출
   unsigned short fracB = b & 0x3FF;       // significand bits 추출
 
-  unsigned short s_out = 0;
-  unsigned short exp_out = 0;
-  unsigned short frac_out = 0;
+  if ((expA == 0b11111 && fracA) || (expB == 0b11111 && fracB)) return HPFP_NAN; // NaN 처리
+  if (expA == 0b11111) return a; // a가 무한대일 경우
+  if (expB == 0b11111) return b; // b가 무한대일 경우  
 
-  unsigned short output = 0;
-
-  int i;
-  // specialized
-  if ((expA == 0b11111 && fracA != 0) || (expB == 0b11111 && fracB != 0))
-  {                            // a, b 둘 중 하나 NaN
-    return 0b0111110000000001; // NaN
-  }
-
-  if ((expA == 0b11111 && fracA == 0) || (expB == 0b11111 && fracB == 0))
-  { // a, b 둘 중 하나 이상 무한대
-    if (expA == expB)
-    { // 둘 다 무한대
-      if (sA == sB)
-      { // 부호 같음
-        if (sA == 0)
-          return 0b0111110000000000; // 둘 다 양의 무한대
-        else
-          return 0b1111110000000000; // 둘 다 음의 무한대
-      }
-      else
-      {                            // sA != sB, 부호 다름
-        return 0b0111110000000001; // NaN
-      }
-    }
-    else
-    { // 둘 중 하나만 무한대
-      if (sA == 0 && expA == 0b11111 && fracA == 0)
-      { // a가 양의 무한대
-        if (expB == 0b11111 && fracB != 0)
-        { // b가 NaN
-          return 0b0111110000000001;
-        }
-        else
-        { // b가 NaN이 아님
-          return 0b0111110000000000;
-        }
-      }
-      else if (sA == 1 && expA == 0b11111 && fracA == 0)
-      { // a가 음의 무한대
-        if (expB == 0b11111 && fracB != 0)
-        { // b가 NaN
-          return 0b0111110000000001;
-        }
-        else
-        { // b가 NaN이 아님
-          return 0b1111110000000000;
-        }
-      }
-      else if (sB == 0 && expB == 0b11111 && fracB == 0)
-      { // b가 양의 무한대
-        if (expA == 0b11111 && fracA != 0)
-        { // a가 NaN
-          return 0b0111110000000001;
-        }
-        else
-        { // a가 NaN이 아님
-          return 0b0111110000000000;
-        }
-      }
-      else
-      { // b가 음의 무한대
-        if (expA == 0b11111 && fracA != 0)
-        { // a가 NaN
-          return 0b0111110000000001;
-        }
-        else
-        { // a가 NaN이 아님
-          return 0b1111110000000000;
-        }
-      }
-    }
-  }
-
-  // normalized
-  // hpfp의 exp 값을 통해 E 값 추출
+  // 지수 값 
   int Ea = expA - 15;
   int Eb = expB - 15;
 
-  int E_sub = 0;
-  int fp_sub = 0; // a, b 두 값의 소수점 자리수 차
+  // 정규화된 값이면 Mantissa에 leading 1 추가 
+  fracA |= (expA ? 1 << 10 : 0);
+  fracB |= (expB ? 1 << 10 : 0);
 
-  unsigned short frac_tempA = 0;
-  unsigned short frac_tempB = 0;
-
-  // leading 1 붙여서 frac 값 꺼내오기
-  frac_tempA = fracA + (1 << 10);
-  frac_tempB = fracB + (1 << 10);
-
-  // denormalized
-  if (expA == 0b00000)
-    frac_tempA = fracA;
-  if (expB == 0b00000)
-    frac_tempB = fracB;
-
-  // E값 비교
-  if (Ea == Eb)
-  {                 // 두 값의 지수가 같음
-    exp_out = expA; // 일단 초기값 저장
-  }
-  else
-  { // 두 값의 지수가 다름
-    if (Ea > Eb)
-    {
-      // E값의 차이를 계산
-      E_sub = Ea - Eb;
-      // E가 더 작은 변수의 E값 및 exp 값 수정
-      Eb = Ea;
-      expB = expA;
-      exp_out = expA; // 일단 초기값 저장
-
-      // 지수가 더 컸던 수는 그대로 두고
-      // 지수가 더 작았던 수는 계산했던 E의 값의 차이만큼 shift right
-      frac_tempB >>= E_sub;
-    }
-    else
-    { // Ea < Eb
-      // E값의 차이를 계산
-      E_sub = Eb - Ea;
-      // E가 더 작은 변수의 E값 및 exp 값 수정
-      Ea = Eb;
-      expA = expB;
-      exp_out = expB; // 일단 초기값 저장
-
-      // 지수가 더 컸던 수는 그대로 두고
-      // 지수가 더 작았던 수는 계산했던 E의 값의 차이만큼 shift right
-      frac_tempA >>= E_sub;
-    }
-  }
-  // 부호에 따라 연산 수행
-  unsigned short frac_add = 0;
-  if (sA == sB)
-  {
-    if (sA == 0)
-    { // 둘 다 양수
-      s_out = 0;
-      // 덧셈연산
-      frac_add = frac_tempA + frac_tempB;
-    }
-    else
-    { // 둘 다 음수
-      s_out = 1;
-      // 덧셈 연산
-      frac_add = frac_tempA + frac_tempB;
-    }
-  }
-  else
-  { // sA != sB
-    if (sA == 0)
-    {
-      if (fracA > fracB)
-      { // 절대값 비교
-        s_out = 0;
-        // 뺄셈 연산
-        frac_add = frac_tempA - frac_tempB;
-      }
-      else
-      { // Ea < Eb
-        s_out = 1;
-        // 뺄셈 연산
-        frac_add = frac_tempB - frac_tempA;
-      }
-    }
-    else
-    { // sB == 0
-      if (fracA > fracB)
-      { // 절대값 비교
-        s_out = 1;
-        // 뺄셈 연산
-        frac_add = frac_tempA - frac_tempB;
-      }
-      else
-      { // Ea < Eb
-        s_out = 0;
-        // 뺄셈 연산
-        frac_add = frac_tempB - frac_tempA;
-      }
-    }
+  // 지수 및 Mantissa 정렬 
+  if (Ea > Eb) {
+    fracB >>= (Ea - Eb);
+    expB = expA;
+  } else if (Ea < Eb) {
+    fracA >>= (Eb - Ea);
+    expA = expB;
   }
 
-  // 연산되어 나온 값을 가지고 normalize (frac_out 값 구하기)
-  int count = 0;
-  for (i = sizeof(unsigned short) * 8 - 1; i >= 0; i--)
-  { // MSB부터 시작
-    count++;
-    if (((frac_add >> i) & 1) == 1) // num의 i번째 비트를 추출하는 식{
-      break;
+  // 부호에 따라 연산 수행 
+  unsigned short s_out = sA; // 부호 일단 sA
+  unsigned short frac_out = (sA == sB) ? (fracA + fracB) : (fracA > fracB ? fracA - fracB : fracB - fracA);
+  // 부호가 같으면 덧셈, 부호가 다르면 절대값 큰 값에서 작은 값을 뺌 
+  if (fracA < fracB) s_out = sB; // 큰 쪽의 부호를 유지 
+
+  // normailzation
+  while (frac_out && !(frac_out & (1 << 10))) {
+    frac_out <<= 1;
+    expA--;
   }
+  frac_out &= 0x3FF; // Mantissa는 10비트만 사용하므로 상위 비트 삭제
 
-  for (i = 0; i <= sizeof(unsigned short) * 8 - 1 - count; i++)
-  {
-    // LSB부터 1바로 오른쪽까지 진행
-    frac_out |= (((frac_add >> i) & 1) << i);
-  }
+  // 최종 hpfp값 반환
+  return (s_out << 15) | ((expA + 15) << 10) | frac_out;
 
-  int sft = 10 - (16 - count);
-  if (sft >= 0)
-  {
-    frac_out = frac_out << (-6 + count);
-  }
-  else
-    frac_out = frac_out >> (6 - count);
-
-  fp_sub = 6 - count;
-  exp_out = exp_out + fp_sub;
-
-  output |= (s_out << 15);
-  output |= (exp_out << 10);
-  output |= frac_out;
-
-  return output;
-  // leading 1 붙여서 frac 값 꺼내오기
-  // 지수가 더 컸던 수는 그대로 두고
-  // 지수가 더 작았던 수는 계산했던 E의 값의 차이만큼 shift right
-  // 덧셈연산
-  // 연산되어 나온 값을 가지고 normalize (leading 1 고려해 output hpfp의 frac 계산)
-  // sign bit 저장
-  // output hpfp의 지수값 계산
-  // frac 라운딩 거쳐서 최종 hpfp 값 반환
 }
 
 hpfp multiply_function(hpfp a, hpfp b)
